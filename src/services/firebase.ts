fa21906bb3d53c8841e14251525dba3620dbe867
Uallesson <uallessons@gmail.com>
2021-06-21 23:40:58 -0300

    context and hooks

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,19 +3,20 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-var firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGE_SENDER,
-    appId: process.env.APP_ID,
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
-
 
 firebase.initializeApp(firebaseConfig)
 
-export const auth = firebase.auth()
-export const database = firebase.database()
+const auth = firebase.auth()
+const database = firebase.database()
+
+export { firebase, auth, database }
 
